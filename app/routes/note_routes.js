5cf30b527f69721f29e993ebc2369b1cd948ab33
Lucas Tagliani <lucas.tagliani@outlook.com>
2019-06-10 08:50:25 -0300

    feat: create post and get endpoints

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,28 @@
+var ObjectID = require('mongodb').ObjectID;
 module.exports = function (app, db) {
     app.post('/notes', (req, res) => {
-        // You'll create your note here.    
-        console.log(req.body)
-        res.send('Hello')
+
+        const note = { text: req.body.body, title: req.body.title }
+
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
+
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+
+        db.collection('notes').findOne(details, (error, item) => {
+            if (error) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(item);
+            }
+        });
     });
 };
--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app = express();
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-require('./app/routes')(app, {});
 
+MongoClient.connect(db.url, (error, database) => {  
+    if (error) {
+        return console.log(error)                        
+    }
 
-app.listen(port, () => {  
-    console.log('Started on port',  port);
-});
+    // Make sure you add the database name and not the collection name  
+    const notesdb = database.db("notes-db")  
+    require('./app/routes')(app, notesdb);
 
+    app.listen(port, () => {
+        console.log('Started on port', port);
+    });
+})
+// MongoClient.connect(db.url, (error, database) => {
+//     if (error) {
+//         return console.log(error)
+//     }
+
+//     require('./app/routes')(app, database);
+
+//     app.listen(port, () => {
+//         console.log('Started on port', port);
+//     });
+// })
